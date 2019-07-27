a89043ba049d9add1c7a3c412bc550a1eabbc407
Christopher-Robin <jecebbi@gmail.com>
2019-07-27 22:37:14 +1000

    Finished Implementing Vectors and Matricies

--- atyp_Vector2.h
+++ atyp_Vector2.h
@@ -15,130 +15,98 @@ public:
 		};
 		float data[2];
 	};
-	Vector2();
-	Vector2(float, float);
 
-	Vector2 copy() const;
-	Vector2 inverse();
+	Vector2() {
+		memset(data, 0, sizeof(float) * 2);
+	}
 
-	float magnitude();
-	float magnitudeSqr();
-	Vector2& normalise();
+	Vector2(float a_x, float a_y) {
+		x = a_x;
+		y = a_y;
+	}
 
-	float dot(const Vector2& rhs);
-	Vector2 right();
 
-	Vector2 operator+(const Vector2& rhs);
-	Vector2 operator-(const Vector2& rhs);
-	Vector2 operator*(float rhs);
-	Vector2 operator/(float rhs);
+	Vector2 copy() const {
+		return Vector2(x, y);
+	}
 
-	Vector2& operator+=(const Vector2& rhs);
-	Vector2& operator-=(const Vector2& rhs);
-	Vector2& operator*=(float rhs);
-	Vector2& operator/=(float rhs);
+	Vector2 inverse(){
+		return Vector2(-x, -y);
+	}
 
-	float& operator[](int index);
+	float magnitude() {
+		return sqrtf(x * x + y * y);
+	}
 
-	operator float*();
+	float magnitudeSqr() {
+		return x * x + y * y;
+	}
+
+	Vector2& normalise() {
+		float mag = magnitude();
+		assert(mag != 0.0f && "Magnitude is 0. Cannot Divide");
+		x /= mag;
+		y /= mag;
+		return *this;
+	}
+
+	float dot(const Vector2& rhs) {
+		return x * rhs.x + y * rhs.y;
+	}
+
+	Vector2 right() {
+		return Vector2(y, -x);
+	}
+
+	Vector2 operator+(const Vector2& rhs) {
+		return Vector2(x + rhs.x, y + rhs.y);
+	}
+
+	Vector2 operator-(const Vector2& rhs) {
+		return Vector2(x - rhs.x, y - rhs.y);
+	}
+
+	Vector2 operator*(float rhs) {
+		return Vector2(x * rhs, y * rhs);
+	}
+
+	Vector2 operator/(float rhs) {
+		return Vector2(x / rhs, y / rhs);
+	}
+
+	Vector2& operator+=(const Vector2& rhs) {
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+
+	Vector2& operator-=(const Vector2& rhs) {
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
+
+	Vector2& operator*=(float rhs) {
+		x *= rhs;
+		y *= rhs;
+		return *this;
+	}
+
+	Vector2& operator/=(float rhs) {
+		x /= rhs;
+		y /= rhs;
+		return *this;
+	}
+
+	float& operator[](int index) {
+		return *((float*)this + index);
+	}
+
+	operator float* () {
+		return (float*)this;
+	}
 };
 
-Vector2 operator*(float lhs, Vector2 rhs);
-
-/*
-
-
-Vector2::Vector2(){
-	memset(data, 0, sizeof(float) * 2);
-}
-
-Vector2::Vector2(float a_x, float a_y){
-	x = a_x;
-	y = a_y;
-}
-
-Vector2 Vector2::copy() const
-{
-	return Vector2(x, y);
-}
-
-Vector2 Vector2::inverse()
-{
-	return Vector2(-x, -y);
-}
-
-float Vector2::magnitude(){
-	return sqrtf(x * x + y * y);
-}
-
-float Vector2::magnitudeSqr(){
-	return x * x + y * y;
-}
-
-Vector2& Vector2::normalise(){
-	float mag = magnitude();
-	assert(mag != 0.0f && "Magnitude is 0. Cannot Divide");
-	x /= mag;
-	y /= mag;
-	return *this;
-}
-
-float Vector2::dot(const Vector2 & rhs){
-	return x * rhs.x + y * rhs.y;
-}
-
-Vector2 Vector2::right(){
-	return Vector2(y, -x);
-}
-
-Vector2 Vector2::operator+(const Vector2 & rhs){
-	return Vector2(x + rhs.x, y + rhs.y);
-}
-
-Vector2 Vector2::operator-(const Vector2 & rhs){
-	return Vector2(x - rhs.x, y - rhs.y);
-}
-
-Vector2 Vector2::operator*(float rhs){
-	return Vector2(x * rhs, y * rhs);
-}
-
-Vector2 Vector2::operator/(float rhs){
-	return Vector2(x / rhs, y / rhs);
-}
-
-Vector2& Vector2::operator+=(const Vector2 & rhs){
-	x += rhs.x;
-	y += rhs.y;
-	return *this;
-}
-
-Vector2& Vector2::operator-=(const Vector2 & rhs){
-	x -= rhs.x;
-	y -= rhs.y;
-	return *this;
-}
-
-Vector2& Vector2::operator*=(float rhs){
-	x *= rhs;
-	y *= rhs;
-	return *this;
-}
-
-Vector2& Vector2::operator/=(float rhs){
-	x /= rhs;
-	y /= rhs;
-	return *this;
-}
-
-float & Vector2::operator[](int index){
-	return *((float*)this + index);
-}
-
-Vector2::operator float*(){
-	return (float*)this;
-}
-
-Vector2 operator*(float lhs, Vector2 rhs){
+Vector2 operator*(float lhs, Vector2 rhs) {
 	return Vector2(rhs.x * lhs, rhs.y * lhs);
 }
--- atyp_Vector3.h
+++ atyp_Vector3.h
@@ -36,135 +36,106 @@ public:
 		float data[3];
 	};
 
-	Vector3();
-	Vector3(float* data);
-	Vector3(float x, float y, float z);
+	Vector3() {
+		x = y = z = 0;
+	}
 
-	float magnitude();
-	float magnitudeSqr();
-	Vector3& normalise();
+	Vector3(float* ptr) {
+		memcpy(data, ptr, sizeof(float) * 3);
+	}
 
-	float dot(const Vector3& rhs);
+	Vector3(float a_x, float a_y, float a_z) {
+		x = a_x;
+		y = a_y;
+		z = a_z;
+	}
 
-	Vector3 copy() const;
+	float magnitude() {
+		return sqrtf(x * x + y * y + z * z);
+	}
 
-	Vector3 operator+(const Vector3& rhs);
-	Vector3 operator-(const Vector3& rhs);
-	Vector3 operator*(float rhs);
-	Vector3 operator/(float rhs);
+	float magnitudeSqr() {
+		return x * x + y * y + z * z;
+	}
 
-	Vector3& operator+=(const Vector3& rhs);
-	Vector3& operator-=(const Vector3& rhs);
-	Vector3& operator*=(float rhs);
-	Vector3& operator/=(float rhs);
+	Vector3& normalise() {
+		float mag = magnitude();
+		x /= mag;
+		y /= mag;
+		z /= mag;
+		return *this;
+	}
 
-	float& operator[](int index);
-	operator float*();
+	float dot(const Vector3& rhs) {
+		return (x * rhs.x + y * rhs.y + z * rhs.z);
+	}
 
-	Vector3 cross(const Vector3&);
+	Vector3 copy() const {
+		return Vector3(x, y, z);
+	}
+
+	Vector3 operator+(const Vector3& rhs) {
+		return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
+	}
+
+	Vector3 operator-(const Vector3& rhs) {
+		return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
+	}
+
+	Vector3 operator*(float rhs) {
+		return Vector3(x * rhs, y * rhs, z * rhs);
+	}
+
+	Vector3 operator/(float rhs) {
+		return Vector3(x / rhs, y / rhs, z / rhs);
+	}
+
+
+	Vector3& operator+=(const Vector3& rhs) {
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+
+	Vector3& operator-=(const Vector3& rhs) {
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
+
+	Vector3& operator*=(float rhs) {
+		x *= rhs;
+		y *= rhs;
+		return *this;
+	}
+
+	Vector3& operator/=(float rhs) {
+		x /= rhs;
+		y /= rhs;
+		return *this;
+	}
+
+	float& operator[](int index) {
+		return *((float*)this + index);
+	}
+
+	operator float* () {
+		return (float*)this;
+	}
+
+	operator Vector2() {
+		return Vector2(x, y);
+	}
+
+	Vector3 cross(const Vector3& other){
+		return Vector3(
+			y * other.z - z * other.y,
+			z * other.x - x * other.z,
+			x * other.y - y * other.x
+		);
+	}
 };
 
-Vector3 operator*(const float, const Vector3&);
-
-/*
-#include <cmath>
-
-
-Vector3::Vector3(){
-	x = y = z = 0;
-}
-
-Vector3::Vector3(float* ptr){
-	memcpy(data, ptr, sizeof(float) * 3);
-}
-
-Vector3::Vector3(float a_x, float a_y, float a_z){
-	x = a_x;
-	y = a_y;
-	z = a_z;
-}
-
-float Vector3::magnitude(){
-	return sqrtf(x * x + y * y + z * z);
-}
-
-float Vector3::magnitudeSqr(){
-	return x * x + y * y + z * z;
-}
-
-Vector3& Vector3::normalise(){
-	float mag = magnitude();
-	x /= mag;
-	y /= mag;
-	z /= mag;
-	return *this;
-}
-
-float Vector3::dot(const Vector3 & rhs){
-	return (x * rhs.x + y * rhs.y + z * rhs.z);
-}
-
-Vector3 Vector3::copy() const{
-	return Vector3(x, y, z);
-}
-
-Vector3 Vector3::operator+(const Vector3 & rhs){
-	return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
-}
-
-Vector3 Vector3::operator-(const Vector3 & rhs){
-	return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
-}
-
-Vector3 Vector3::operator*(float rhs){
-	return Vector3(x * rhs, y * rhs, z * rhs);
-}
-
-Vector3 Vector3::operator/(float rhs){
-	return Vector3(x / rhs, y / rhs, z / rhs);
-}
-
-Vector3 & Vector3::operator+=(const Vector3 & rhs){
-	x += rhs.x;
-	y += rhs.y;
-	return *this;
-}
-
-Vector3 & Vector3::operator-=(const Vector3 & rhs){
-	x -= rhs.x;
-	y -= rhs.y;
-	return *this;
-}
-
-Vector3& Vector3::operator*=(float rhs){
-	x *= rhs;
-	y *= rhs;
-	return *this;
-}
-
-Vector3& Vector3::operator/=(float rhs){
-	x /= rhs;
-	y /= rhs;
-	return *this;
-}
-
-float& Vector3::operator[](int index){
-	return *((float*)this + index);
-}
-
-Vector3::operator float*(){
-	return (float*)this;
-}
-
-Vector3 Vector3::cross(const Vector3& other)
-{
-	return Vector3(
-		y * other.z - z * other.y,
-		z * other.x - x * other.z,
-		x * other.y - y * other.x
-	);
-}
-
-Vector3 operator*(const float n, const Vector3& rhs){
+Vector3 operator*(const float n, const Vector3& rhs) {
 	return Vector3(rhs.x * n, rhs.y * n, rhs.z * n);
 }
